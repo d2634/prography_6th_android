f8d2a2831dbc5a2b05097b7bb5770b6537358b86
d2634 <59246542+d2634@users.noreply.github.com>
2020-03-01 16:22:46 +0900

    fragment
    make fragment

---
+++ app/src/main/java/com/example/prography_6th_android_application/FragmentOne.java
@@ -0,0 +1,21 @@
+package com.example.prography_6th_android_application;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentOne extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view= inflater.inflate(R.layout.frag_layout_1,container,false);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/prography_6th_android_application/MainActivity.java
+++ app/src/main/java/com/example/prography_6th_android_application/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.prography_6th_android_application;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabItem tab1, tab2, tab3;
+    private Context mThis;
+    public PagerAdapter pagerAdapter;
+
+    static boolean tab1_act;
+    static boolean tab3_act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mThis = this;
+        tab1_act = false;
+        tab3_act = false;
+
+        TabLayout tablayout = findViewById(R.id.tablayout);
+        tab1 = findViewById(R.id.Tab1);
+        tab2 = findViewById(R.id.Tab2);
+        tab3 = findViewById(R.id.Tab3);
+        viewPager = findViewById(R.id.viewpager);
+
+        pagerAdapter = new PageAdapter(getSupportFragmentManager(), tablayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+
+        tablayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if(tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                }else if(tab.getPosition()==1){
+                    pagerAdapter.notifyDataSetChanged();
+                }else if(tab.getPosition()==2){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {}
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {}
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tablayout));
     }
 }
